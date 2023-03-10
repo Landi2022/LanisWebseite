7f5e2e3f2b21500aa210af61f03a7e90b533386b
Landi2022 <alexanderdonnikow@gmx.de>
2023-03-10 12:09:52 +0100

    Registration html erstellt

---
+++ ajax/regestration.php
@@ -0,0 +1,8 @@
+<?php
+//anfrage überprüfung && rolle des nutzer überprüfen admin only
+//registration vorbereiten password_hash($values['password'], PASSWORD_DEFAULT);
+//neuen nuter in die datenbank schreiben
+//ergebniss zurück senden
+//fall 1
+//fall 2
+?>
